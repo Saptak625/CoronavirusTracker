4e260c602b6887f6a46e03e0ca5d24441d0359a7
23akarkare <52218621+23akarkare@users.noreply.github.com>
2020-11-14 12:39:50 -0500

    Add files via upload

---
+++ COVIDTracker/COVIDTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVIDTracker
+//
+//  Created by Ayush Karkare on 11/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
