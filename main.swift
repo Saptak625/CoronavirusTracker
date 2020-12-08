af263bf9576c8ae4ac4e9400befe9c165c2cf380
Saptak625 <66028584+Saptak625@users.noreply.github.com>
2020-12-08 17:22:49 -0500

    Delete main.swift

--- main.swift
+++
@@ -1,8 +0,0 @@
-print(User.currentDatabaseId)
-var user1=User()
-print(user1)
-print(user1.riskAssesment)
-user1.evaluateRisk()
-print(user1.riskAssesment)
-user1.checkInfection()
-print(user1.riskAssesment)
